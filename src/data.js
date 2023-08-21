59c83a8ab6985c0a898f7269aa125fc368f1ef38
glitton <glcodeworks@gmail.com>
2023-08-20 19:26:28 -0500

    slider using react slick

--- src/data.js
+++ src/data.js
@@ -79,32 +79,36 @@ export const longList = [
   },
   {
     id: 5,
-    image: "https://www.course-api.com/images/people/person-1.jpeg",
-    name: "maria ferguson",
+    image:
+      "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "mini cooper",
     title: "project manager",
     quote:
       "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
   },
   {
     id: 6,
-    image: "https://www.course-api.com/images/people/person-4.jpeg",
-    name: "john doe",
+    image:
+      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "joe durham",
     title: "the guy",
     quote:
       "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
   },
   {
     id: 7,
-    image: "https://www.course-api.com/images/people/person-3.jpeg",
-    name: "peter smith",
+    image:
+      "https://images.pexels.com/photos/17819118/pexels-photo-17819118/free-photo-of-portrait-of-person-in-short-black-hair-from-profile.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "david wong",
     title: "product manager",
     quote:
       "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
   },
   {
     id: 8,
-    image: "https://www.course-api.com/images/people/person-2.jpeg",
-    name: "susan andersen",
+    image:
+      "https://images.pexels.com/photos/7841717/pexels-photo-7841717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "leslie davis",
     title: "the big boss",
     quote:
       "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
